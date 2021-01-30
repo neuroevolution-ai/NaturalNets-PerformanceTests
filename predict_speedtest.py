bcbead4642a3d7cbe816824779c4e31502f452a5
Daniel Zimmermann <dzimmer@fzi.de>
2021-01-30 16:29:02 -0500

    predict speedtest added

---
+++ predict_speedtest.py
@@ -0,0 +1,49 @@
+import numpy as np
+import time
+
+number_neuron_connections = 3000000
+u = np.random.rand(number_neuron_connections, 1, 6)
+A = np.random.rand(6, 16)
+B = np.random.rand(16, 16)
+C = np.random.rand(16, 16)
+D = np.random.rand(16, 8)
+E = np.random.rand(8, 1)
+
+
+def predict(u):
+    x = np.matmul(u, A)
+    x = np.maximum(0, x)
+    x = np.matmul(x, B)
+    x = np.maximum(0, x)
+    x = np.matmul(x, C)
+    x = np.maximum(0, x)
+    x = np.matmul(x, D)
+    x = np.maximum(0, x)
+    x = np.matmul(x, E)
+
+    return x
+
+
+t1 = time.time()
+
+#cp.cuda.Device(0).synchronize()
+
+x1 = predict(u)
+
+t2 = time.time()
+
+print((t2-t1))
+
+x2 = x1*0
+
+
+t1 = time.time()
+
+#cp.cuda.Device(0).synchronize()
+
+for i in range(number_neuron_connections):
+    x2[i] = predict(u[i])
+
+t2 = time.time()
+
+print((t2-t1))
