df809c762c67bfd8a380a100e6407d16a6417c9a
Daniel Zimmermann <dzimmer@fzi.de>
2021-02-14 06:58:46 -0500

    Some refactoring

--- hyperbrain_plot_neuron_positions.py
+++ hyperbrain_plot_neuron_positions.py
@@ -7,12 +7,12 @@ import matplotlib.animation as animation
 matplotlib.use('Qt5Agg')
 
 
-def get_circle_points(N):
-    circle_x = np.zeros(N)
-    circle_y = np.zeros(N)
-    t = np.linspace(0, 2 * np.pi, N, endpoint=False)
+def get_circle_points(n):
+    circle_x = np.zeros(n)
+    circle_y = np.zeros(n)
+    t = np.linspace(0, 2 * np.pi, n, endpoint=False)
 
-    for i in range(N):
+    for i in range(n):
         circle_x[i] = 0.45*np.cos(t[i])+0.5
         circle_y[i] = 0.45*np.sin(t[i])+0.5
 
@@ -37,9 +37,9 @@ def update_plot(i):
         t_start = time.time()
         print(i)
 
-    scatter_red_inputs.set_array(color_data[i])
-    scatter_green_inputs.set_array(color_data[i])
-    scatter_blue_inputs.set_array(color_data[i])
+    scat_red_inputs.set_array(color_data[i])
+    scat_green_inputs.set_array(color_data[i])
+    scat_blue_inputs.set_array(color_data[i])
 
     if i == number_frames-1:
         print((time.time() - t_start))
@@ -57,30 +57,32 @@ input_image_height = 64
 red_z = 1.3
 green_z = 1.6
 blue_z = 1.9
+number_neurons = 5000
 number_outputs = 16
 outputs_z = -0.5
-
-number_points = input_image_width * input_image_height
-
-color_data = np.random.random((number_frames, number_points))
 
 # Inputs from rgb meshgrids
 rgb_x, rgb_y = get_rgb_points(input_image_width, input_image_height)
 
-# Neurons are randomly places in a cube with length = 1
-neurons_x, neurons_y, neurons_z, color = np.random.random((4, number_points))
+# CTRNN Neurons are randomly placed in a cube with length = 1
+neurons_x, neurons_y, neurons_z = np.random.random((3, number_neurons))
 
-# Outputs as a Circle
+# Outputs as a circle
 outputs_x, outputs_y = get_circle_points(number_outputs)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-scatter_red_inputs = ax.scatter(rgb_x, rgb_y, red_z, c=color, s=15, edgecolors='none', cmap="Reds", alpha=0.7)
-scatter_green_inputs = ax.scatter(rgb_x, rgb_y, green_z, c=color, s=15, edgecolors='none', cmap="Greens", alpha=0.7)
-scatter_blue_inputs = ax.scatter(rgb_x, rgb_y, blue_z, c=color, s=15, edgecolors='none', cmap="Blues", alpha=0.7)
-scatter_neurons = ax.scatter(neurons_x, neurons_y, neurons_z, c="Black", s=15, edgecolors='none')
-scatter_outputs = ax.scatter(outputs_x, outputs_y, outputs_z, c="Orange", s=30, edgecolors='none')
+color_red = np.random.random(input_image_width*input_image_height)
+color_green = np.random.random(input_image_width*input_image_height)
+color_blue = np.random.random(input_image_width*input_image_height)
+color_data = np.random.random((number_frames, input_image_width*input_image_height))
+
+scat_red_inputs = ax.scatter(rgb_x, rgb_y, red_z, c=color_red, s=15, edgecolors='none', cmap="Reds", alpha=0.7)
+scat_green_inputs = ax.scatter(rgb_x, rgb_y, green_z, c=color_green, s=15, edgecolors='none', cmap="Greens", alpha=0.7)
+scat_blue_inputs = ax.scatter(rgb_x, rgb_y, blue_z, c=color_blue, s=15, edgecolors='none', cmap="Blues", alpha=0.7)
+scat_neurons = ax.scatter(neurons_x, neurons_y, neurons_z, c="Black", s=15, edgecolors='none')
+scat_outputs = ax.scatter(outputs_x, outputs_y, outputs_z, c="Orange", s=30, edgecolors='none')
 
 ani = animation.FuncAnimation(fig, update_plot, frames=100, repeat=False, interval=20)
 
@@ -88,7 +90,7 @@ ani = animation.FuncAnimation(fig, update_plot, frames=100, repeat=False, interv
 manager = plt.get_current_fig_manager()
 manager.window.showMaximized()
 
-plt.show()
+# plt.show()
 
 # ani.save('lines.mp4', writer=writer)
 
