aa630caac4776aedc7db8eafa8b03fabd87cd321
Daniel Zimmermann <dzimmer@fzi.de>
2021-02-15 10:10:33 -0500

    More refactoring/renaming

--- hyperbrain_plot_neuron_positions.py
+++ hyperbrain_plot_neuron_positions.py
@@ -8,60 +8,63 @@ import matplotlib.animation as animation
 matplotlib.use('Qt5Agg')
 
 
-def get_rgb_points(width, height, red_z, green_z, blue_z):
+def get_rgb_positions(width: int, height: int,
+                      red_position_z: float, green_position_z: float, blue_position_z: float) -> np.ndarray:
 
     space_x = np.linspace(0, 1, width)
     space_y = np.linspace(0, 1, height)
 
     grid_x, grid_y = np.meshgrid(space_x, space_y)
-    rgb_points_x = grid_x.flatten()
-    rgb_points_y = grid_y.flatten()
+    rgb_positions_x = grid_x.flatten()
+    rgb_positions_y = grid_y.flatten()
 
-    positions_red = np.column_stack((rgb_points_x, rgb_points_y, np.ones(width*height) * red_z))
-    positions_green = np.column_stack((rgb_points_x, rgb_points_y, np.ones(width*height) * green_z))
-    positions_blue = np.column_stack((rgb_points_x, rgb_points_y, np.ones(width*height) * blue_z))
+    red_positions = np.column_stack((rgb_positions_x, rgb_positions_y, np.ones(width*height) * red_position_z))
+    green_positions = np.column_stack((rgb_positions_x, rgb_positions_y, np.ones(width*height) * green_position_z))
+    blue_positions = np.column_stack((rgb_positions_x, rgb_positions_y, np.ones(width*height) * blue_position_z))
 
-    return np.vstack((positions_red, positions_green, positions_blue))
-
-
-def get_circle_points(n, position_z):
-
-    circle_x = np.zeros(n)
-    circle_y = np.zeros(n)
-    t = np.linspace(0, 2 * np.pi, n, endpoint=False)
-
-    for i in range(n):
-        circle_x[i] = 0.45*np.cos(t[i])+0.5
-        circle_y[i] = 0.45*np.sin(t[i])+0.5
-
-    circle_z = np.ones(n)*position_z
-
-    return np.column_stack((circle_x, circle_y, circle_z))
+    return np.vstack((red_positions, green_positions, blue_positions))
 
 
-def get_rgb_from_observation(ob):
+def get_circle_positions(number_positions: int, radius: float, position_z: float) -> np.ndarray:
 
-    ob_scaled = ob/255.0
+    circle_positions_x = np.zeros(number_positions)
+    circle_positions_y = np.zeros(number_positions)
 
-    ob_red = ob_scaled[:, :, 0].flatten()
-    ob_green = ob_scaled[:, :, 1].flatten()
-    ob_blue = ob_scaled[:, :, 2].flatten()
+    angles = np.linspace(0, 2 * np.pi, number_positions, endpoint=False)
 
-    return np.concatenate((ob_red, ob_green, ob_blue))
+    for i in range(number_positions):
+        circle_positions_x[i] = radius*np.cos(angles[i])+0.5
+        circle_positions_y[i] = radius*np.sin(angles[i])+0.5
+
+    circle_positions_z = np.ones(number_positions) * position_z
+
+    return np.column_stack((circle_positions_x, circle_positions_y, circle_positions_z))
 
 
-def split_coordinates(coordinates):
+def get_input_vector_from_observation(observation: np.ndarray) -> np.ndarray:
 
-    return coordinates[:, 0], coordinates[:, 1], coordinates[:, 2]
+    observation_scaled = observation / 255.0
+
+    observation_red = observation_scaled[:, :, 0].flatten()
+    observation_green = observation_scaled[:, :, 1].flatten()
+    observation_blue = observation_scaled[:, :, 2].flatten()
+
+    return np.concatenate((observation_red, observation_green, observation_blue))
 
 
-def update_plot(k):
+def place_points(points: np.ndarray, c, s, cmap):
+
+    points_x = points[:, 0]
+    points_y = points[:, 1]
+    points_z = points[:, 2]
+
+    return ax.scatter(points_x, points_y, points_z, c=c, s=s, edgecolors='none', cmap=cmap, alpha=0.9)
+
+
+def update_plot(k: int) -> None:
 
     obs, rew, done, info = env.step(env.action_space.sample())
-    inputs = get_rgb_from_observation(obs)
-    red = inputs[0:image_size]
-    green = inputs[image_size:2 * image_size]
-    blue = inputs[2 * image_size:3 * image_size]
+    input_data = get_input_vector_from_observation(obs)
 
     print(k)
 
@@ -70,8 +73,12 @@ def update_plot(k):
         t_start = time.time()
 
     if unified_inputs:
-        scat_inputs.set_array(inputs)
+        scat_inputs.set_array(input_data)
     else:
+        red = input_data[0:image_size]
+        green = input_data[image_size:2 * image_size]
+        blue = input_data[2 * image_size:3 * image_size]
+
         scat_red_inputs.set_array(red)
         scat_green_inputs.set_array(green)
         scat_blue_inputs.set_array(blue)
@@ -83,73 +90,66 @@ def update_plot(k):
 
 
 # Parameters
-environment = "procgen:procgen-heist-v0"
-memory_mode = True
-number_frames = 1000
-input_image_width = 64
-input_image_height = 64
-red_z = 2
-green_z = 3
-blue_z = 4
-number_neurons = 5000
-number_outputs = 16
-outputs_z = -1
-record_video = False
-unified_inputs = False
+environment: str = "procgen:procgen-heist-v0"
+memory_mode: bool = True
+number_frames: int = 1000
+input_image_width: int = 64
+input_image_height: int = 64
+red_z: float = 2.0
+green_z: float = 3.0
+blue_z: float = 4.0
+number_neurons: int = 5000
+number_outputs: int = 16
+outputs_radius: float = 0.45
+outputs_z: float = -1.0
+record_video: bool = False
+unified_inputs: bool = False
+
 
 # Input positions
-image_size = input_image_width * input_image_height
-input_positions = get_rgb_points(input_image_width, input_image_height, red_z, green_z, blue_z)
-inputs_red_positions = input_positions[0:image_size, :]
-inputs_green_positions = input_positions[image_size:2 * image_size, :]
-inputs_blue_positions = input_positions[2 * image_size:3 * image_size, :]
+input_positions = get_rgb_positions(input_image_width, input_image_height, red_z, green_z, blue_z)
 
-# CTRNN Neurons are randomly placed in a cube with length = 1
-neurons_positions = np.random.random((number_neurons, 3))
+# CTRNN neurons are randomly placed in a cube with length = 1
+neuron_positions = np.random.random((number_neurons, 3))
 
 # Outputs as a circle
-outputs_positions = get_circle_points(number_outputs, outputs_z)
+output_positions = get_circle_positions(number_outputs, outputs_radius, outputs_z)
 
 if memory_mode:
     env = gym.make(environment, distribution_mode="memory")
 else:
     env = gym.make(environment)
 
-# Reset Procgen Env and get initial observation
-obs_init = env.reset()
-inputs_init = get_rgb_from_observation(obs_init)
-red_init = inputs_init[0:image_size]
-green_init = inputs_init[image_size:2 * image_size]
-blue_init = inputs_init[2 * image_size:3 * image_size]
-
-# Split all positions from 2D matrizes to 1D vectors (required for scatter function)
-inputs_x, inputs_y, inputs_z = split_coordinates(input_positions)
-inputs_red_x, inputs_red_y, inputs_red_z = split_coordinates(inputs_red_positions)
-inputs_green_x, inputs_green_y, inputs_green_z = split_coordinates(inputs_green_positions)
-inputs_blue_x, inputs_blue_y, inputs_blue_z = split_coordinates(inputs_blue_positions)
-neurons_x, neurons_y, neurons_z = split_coordinates(neurons_positions)
-outputs_x, outputs_y, outputs_z = split_coordinates(outputs_positions)
+# Reset Procgen env and get initial observation
+observation_init = env.reset()
+input_data_init = get_input_vector_from_observation(observation_init)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Place RGB inputs
 if unified_inputs:
-    scat_inputs = ax.scatter(inputs_x, inputs_y, inputs_z, c=np.concatenate((red_init, green_init, blue_init)), s=15,
-                             edgecolors='none', cmap="Greys", alpha=0.9)
+    scat_inputs = place_points(input_positions, c=input_data_init, s=15, cmap="Greys")
 else:
-    scat_red_inputs = ax.scatter(inputs_red_x, inputs_red_y, inputs_red_z, c=red_init, s=15,
-                                 edgecolors='none', cmap="Reds", alpha=0.9)
-    scat_green_inputs = ax.scatter(inputs_green_x, inputs_green_y, inputs_green_z, c=green_init, s=15,
-                                   edgecolors='none', cmap="Greens", alpha=0.9)
-    scat_blue_inputs = ax.scatter(inputs_blue_x, inputs_blue_y, inputs_blue_z, c=blue_init, s=15,
-                                  edgecolors='none', cmap="Blues", alpha=0.9)
+    image_size = input_image_width * input_image_height
+
+    input_data_red_init = input_data_init[0:image_size]
+    input_data_green_init = input_data_init[image_size:2 * image_size]
+    input_data_blue_init = input_data_init[2 * image_size:3 * image_size]
+
+    input_positions_red = input_positions[0:image_size, :]
+    input_positions_green = input_positions[image_size:2 * image_size, :]
+    input_positions_blue = input_positions[2 * image_size:3 * image_size, :]
+
+    scat_red_inputs = place_points(input_positions_red, c=input_data_red_init, s=15, cmap="Reds")
+    scat_green_inputs = place_points(input_positions_green, c=input_data_green_init, s=15, cmap="Greens")
+    scat_blue_inputs = place_points(input_positions_blue, c=input_data_blue_init, s=15, cmap="Blues")
 
 # Place CTRNN neurons
-scat_neurons = ax.scatter(neurons_x, neurons_y, neurons_z, c="Black", s=15, edgecolors='none')
+scat_neurons = place_points(neuron_positions, c="Black", s=15, cmap=None)
 
 # Place outputs
-scat_outputs = ax.scatter(outputs_x, outputs_y, outputs_z, c="Orange", s=30, edgecolors='none')
+scat_outputs = place_points(output_positions, c="Orange", s=30, cmap=None)
 
 ani = animation.FuncAnimation(fig, update_plot, frames=number_frames, repeat=False, interval=20)
 
@@ -160,7 +160,7 @@ manager.window.showMaximized()
 if record_video:
     # Set up formatting for the movie files
     Writer = animation.writers['ffmpeg']
-    writer = Writer(fps=3, metadata=dict(artist='Me'), bitrate=-1)
+    writer = Writer(fps=30, metadata=dict(artist='Me'), bitrate=-1)
 
     ani.save('hyperbrain.mp4', writer=writer)
 
